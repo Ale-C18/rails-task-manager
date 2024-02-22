8c9ad8cb9618b5b3bfc01b6da72a71b293b87e04
Jose Carrillo <jacg182010@gmail.com>
2024-02-22 17:44:24 -0600

    ejercicio 2 act

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks' => 'tasks#index', as: :tasks
-  get 'tasks/new' => 'tasks#new', as: :new
-  get 'tasks/:id' => 'tasks#show', as: :task
-  post 'tasks' => 'tasks#create', as: :create
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/edit/:id', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
